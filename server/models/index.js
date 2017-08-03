a1c4c7aafc94afa25428408bd31c3dc0d0f05efb
mkc1 <coulism@gmail.com>
2017-08-03 16:26:12 -0400

    initial models

--- server/models/index.js
+++ server/models/index.js
@@ -1,1 +1,68 @@
-index.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/hunch');
+
+
+const UserSchema = new Schema({
+    name: {
+        type: String
+    },
+    points: {
+        type: Number
+    },
+    answer: { 
+        type: Schema.Types.ObjectId,
+        ref: 'Answer'
+    },
+    selection: { 
+        type: Schema.Types.ObjectId,
+        ref: 'Selection'
+    },
+})
+
+const GameSchema = new Schema({
+    currentQuestion: { 
+        type: Schema.Types.ObjectId,
+        ref: 'Question' },
+    round: {
+        type: Number
+    },
+    users: [UserSchema],
+    questions: [QuestionSchema]
+})
+
+const QuestionSchema = new Schema({
+    question: {
+        type: String
+    }
+})
+
+const AnswerSchema = new Schema({
+    question: {
+        type: Schema.Types.ObjectId,
+        ref: 'Question'
+    },
+    answer: {
+        type: String 
+    }
+})
+
+const SelectionSchema = new Schema({
+    question: {
+        type: Schema.Types.ObjectId,
+        ref: 'Question'
+    },
+    suspected_user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+})
+
+module.exports = {
+    Game: GameSchema,
+    User: UserSchema,
+    Question: QuestionSchema,
+    Answer: AnswerSchema,
+    Selection: SelectionSchema
+}
