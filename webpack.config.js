da13e0e8a5297f1c251eff35ebe1e01919489d0b
mkc1 <coulism@gmail.com>
2017-09-13 17:04:34 -0400

    webpack started

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './public/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: './public/main.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'main.bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig],
+  devServer: {
+    port: 3000
+  },
+}
