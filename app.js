8def340af1b0331893bfe5ceeff33d561b3028b6
mkc1 <coulism@gmail.com>
2017-09-15 19:41:51 -0400

    js folder

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const io = require('./server/io/socket')(server);
 const bodyParser = require('body-parser');
 const path = require('path');
 const router = require('./server/routes/');
@@ -9,15 +9,15 @@ const router = require('./server/routes/');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
 app.use('/', router);
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, ()=>{
   console.log('Example app listening on port 3000!');
-});
-
-io.on('connection', function (socket) {
-  console.log('socket io connection success')
 });
