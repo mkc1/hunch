260ca37ffb824c899a2930d8a041a6462e7eb42f
mkc1 <coulism@gmail.com>
2017-09-19 15:54:43 -0400

    backend changes

--- server/io/socket.js
+++ server/io/socket.js
@@ -4,7 +4,33 @@ module.exports = (server) => {
     var io = socketio(server);
 
     io.on('connection', (socket)=>{
-        console.log('socket io connection success')
+        console.log('socket io connection success?');
+
+        socket.on('join-room', function (data) {
+            const gameCode = data.code;
+            socket.join(gameCode);
+            console.log('just joined', socket.id, data.name);
+            io.to(gameCode).emit('joined', data);
+        });
+
+        socket.on('get-current-players', function(gameCode){
+            // console.log('trying to get players', gameCode);
+            var clients = io.sockets.adapter.rooms[gameCode].sockets;
+            var players = [];
+            for (var key in clients) {
+                if (clients.hasOwnProperty(key)) {
+                    players.push(key);
+                };
+            };
+            console.log(players, gameCode)
+            io.to(gameCode).emit('players', players);
+        })
+
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+        });
+
+
     });
 
     return io;
