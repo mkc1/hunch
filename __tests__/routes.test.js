2ebcc96da06cb7b92292af2f8704268d2f28965b
mkc1 <coulism@gmail.com>
2017-09-27 00:26:47 -0400

    more routes

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -4,14 +4,16 @@ const request = require('supertest')(app);
 const mongoose = require('mongoose');
 const Game = require('./../server/models/index').Game;
 const User = require('./../server/models/index').User;
+const Answer = require('./../server/models/index').Answer;
 const Topic = require('./../server/models/index').Topic;
+const topics = require('./../server/seed/topics.js');
 
 
 beforeEach((done) => {
     for (var i in mongoose.connection.collections) {
-      mongoose.connection.collections[i].remove(function() {});
+      mongoose.connection.collections[i].remove(()=>{});
     }
-    console.log('done first')
+    Topic.insertMany(topics);
     return done();
 });
 
@@ -36,4 +38,86 @@ test('POST api', (done) => {
         expect(response.body.users[0].name).toBe('testname1!')
         done();
     });
+});
+
+describe('testing answers', () => {
+
+    let game1;
+    let user1;
+    let user2;
+    beforeEach((done) => {
+    return Promise.all([
+            User.create([{name: 'testname1!'}, {name: 'testname2!', _id: '59cb0b4e6618cd7e50788c11'}]),
+            Topic.find({})
+        ])
+        .then(result =>{
+            let newGame = new Game({
+                users: result[0],
+                topics: result[1]
+            });
+            return newGame.save();
+        })
+        .then((game)=>{
+            return Game.findOneAndUpdate({_id: game._id},
+            {$addToSet: {answers: { answer: 'this is the initial answer', user: '59cb0b4e6618cd7e50788c11'}}},
+            {'new': true})
+        })
+        .then(game =>{
+            console.log('the game', game)
+            game1 = game;
+            user1 = game.users[0];
+            user2 = game.users[1];
+            done();
+        })
+        .catch(error =>{
+            console.log('error', error)
+        })
+    });
+
+    test('POST answers', (done) => {
+        console.log('user1', user1);
+        request.post('/answer')
+        .send({
+            game: game1._id,
+            answer: 'a test answer',
+            user: user1._id
+        })
+        .then(response =>{
+            expect(response.statusCode).toBe(201);
+            return Game.findById(game1._id)
+        })
+        .then(game =>{
+            expect(game.answers.length).toBe(2);
+            done();
+        })
+    });
+
+    test('add selection to answers', (done) => {
+
+        console.log('answer id?', game1.answers[0]._id)
+
+        request.put('/answer/' + game1.answers[0]._id)
+        .send({
+            guessing_user: user1._id,
+            suspected_user: user2._id
+        })
+        .then(response => {
+            let newSelections = response.body.answers[0].selections
+            expect(response.statusCode).toBe(201);
+            expect(newSelections.length).toBe(1);
+
+            return Game.findById(game1._id).populate('answers.selections')
+        })
+        .then(game =>{
+            console.log(game);
+            let answer = game.answers.id(game1.answers[0]._id);
+            console.log(answer.populate('selections'))
+            // expect(selections.length).toBe(1);
+            // console.log(selections)
+        })
+        .then(answer =>{
+            console.log(answer)
+            done();
+        })
+    })
 });
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,22 +2,66 @@ const router = require('express').Router();
 const Game = require('./../models/index').Game;
 const User = require('./../models/index').User;
 const Topic = require('./../models/index').Topic;
+const Answer = require('./../models/index').Answer;
 
 router.post('/game', (req, res, next) => {
 
-    return Promise.all([User.create(req.body.users), Topic.find({})])
+    return Promise.all([User.create(req.body), Topic.find({})])
     .then(result =>{
-        console.log('topics', result)
         var newGame = new Game({ users: result[0], topics: result[1] });
         return newGame.save();
     })
-    .then((game)=>{
+    .then(game =>{
         res.status(201).json(game);
     })
-    .catch(error=>{
-        console.log('error', error)
+    .catch(error =>{
+        console.log('error', error);
     })
-
 });
+
+router.post('/answer', (req, res, next) => {
+
+    return Game.findOneAndUpdate({_id: req.body.game},
+            {$addToSet: {answers: { answer: req.body.answer, user: req.body.user}}},
+            {'new': true})
+    .then(game =>{
+        res.sendStatus(201);
+    })
+    .catch(error =>{
+        console.log('error', error);
+    })
+});
+
+// router.post('/answer', (req, res, next) => {
+
+//     return Answer.create({answer: req.body.answer, user: req.body.user})
+//     .then(answer =>{
+//         return answer.populate('user');
+//     })
+//     .then(answer =>{
+//         res.status(201).json(answer);
+//     })
+//     .catch(error =>{
+//         console.log('error', error);
+//     })
+// });
+
+router.put('/answer/:id', (req, res, next) => {
+    return Game.findOne({'answers._id': req.params.id})
+    .then(game =>{
+        // game.answers = game.answers.map(answer =>{
+        //     if (answer._id.toString()===req.params.id) {
+        //         answer.selections.push(req.body.selection)
+        //     }
+        //     return answer;
+        // })
+        game.answers.id(req.params.id).selections.push(req.body)
+        return game.save()
+    })
+    .then(game =>{
+        console.log('saved game?????', game)
+        res.status(201).send(game);
+    })
+})  
 
 module.exports = router;
